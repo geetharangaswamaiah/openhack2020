ad166e30abc4bcc5b09a911fd5c65d2411c9125e
Geetha Rangaswamaiah <geetharangaswamaiah@gmail.com>
2020-02-05 11:49:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,3 +35,14 @@ csv_buffer = StringIO()
 df.to_csv(csv_buffer)
 # s3_resource = boto3.resource('s3')
 s3_resource.Object(bucket, 'df.csv').put(Body=csv_buffer.getvalue())
+
+# Read csv file from s3
+client = boto3.client('s3', aws_access_key_id='AKIA5UBFMXG4DJITDQOB',
+        aws_secret_access_key='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')
+object_key = 'df.csv'
+csv_obj = client.get_object(Bucket = 'machinetrainers', Key=object_key)
+body = csv_obj['Body']
+csv_string = body.read().decode('utf-8')
+
+df = pd.read_csv(StringIO(csv_string))
+print(df)
