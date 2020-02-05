823a92917e917050f056a47be456edecec47f0c7
Geetha Rangaswamaiah <geetharangaswamaiah@gmail.com>
2020-02-05 13:04:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,30 +13,27 @@ import csv
 import threading
 
 # Create csv file in openshift
-print('Creating training_dataset.csv file...')
+print('Creating training_dataset.csv file\n')
 with open('training_dataset.csv', 'w', newline='') as file:
 	writer = csv.writer(file)
 	writer.writerow(["timestamp", "apiload", "memoryusage", "cpu", "podid", "clusterid"])
   
 # Connect to s3 bucket
-print('Connecting to s3 bucket...')
+print('Connecting to s3 bucket\n')
 session = boto3.Session(
     aws_access_key_id = 'AKIA5UBFMXG4DJITDQOB',
     aws_secret_access_key = '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'
 )
 s3_resource = session.resource('s3')
-bucket = s3_resource.get_bucket('machinetrainers')
-key = boto.s3.key.Key(bucket, 'training_dataset.csv')
 
-def printit():
+def myfunction():
 	threading.Timer(10.0, printit).start()
-	print('Appending new data to file...')
+	print('Appending new data to file\n')
 	with open('training_dataset.csv', 'a', newline='') as file:
 		writer = csv.writer(file)
 		writer.writerow(["2020-02-23 7:00:10", 5111.11, 765.92, 3.74, "10.11.12.13", "14.15.16.17"])
-	print('Updating file on s3 bucket...')
-	with open('training_dataset.csv') as f:
-    		key.send_file(f)
+	print('Updating file on s3 bucket\n')
+	s3_resource.Object('machinetrainers','training_dataset.csv').upload_file(Filename='training_dataset.csv')
 	# Read csv file from s3
 	client = boto3.client('s3', aws_access_key_id='AKIA5UBFMXG4DJITDQOB',
         	aws_secret_access_key='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')
@@ -45,42 +42,8 @@ def printit():
 	body = csv_obj['Body']
 	csv_string = body.read().decode('utf-8')
 	df = pd.read_csv(StringIO(csv_string))
-	print('File updated!')
+	print('File updated!\n')
 	print(df)
 	
 
-printit()
-
-
-
-# print('Write to s3 bucket')
-# # Data to write
-# with open('training_dataset.csv', 'a', newline='') as file:
-# 	writer = csv.writer(file)
-# 	writer.writerow(["timestamp", "apiload", "memoryusage", "cpu", "podid", "clusterid"])
-# 	writer.writerow(["2020-02-23 7:00:10", 5111.11, 765.92, 3.74, "10.11.12.13", "14.15.16.17"])
-# 	writer.writerow(["2020-02-23 8:00:10", 6111.11, 865.92, 4.74, "10.11.12.13", "14.15.16.17"])
-
-# # Connect to s3 bucket
-# session = boto3.Session(
-#     aws_access_key_id = 'AKIA5UBFMXG4DJITDQOB',
-#     aws_secret_access_key = '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'
-# )
-# s3_resource = session.resource('s3')
-# # Write to csv file
-# bucket = 'machinetrainers' # already created on S3
-# csv_buffer = StringIO()
-# df.to_csv(csv_buffer)
-# # s3_resource = boto3.resource('s3')
-# s3_resource.Object(bucket, 'df.csv').put(Body=csv_buffer.getvalue())
-
-# # Read csv file from s3
-# client = boto3.client('s3', aws_access_key_id='AKIA5UBFMXG4DJITDQOB',
-#         aws_secret_access_key='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')
-# object_key = 'df.csv'
-# csv_obj = client.get_object(Bucket = 'machinetrainers', Key=object_key)
-# body = csv_obj['Body']
-# csv_string = body.read().decode('utf-8')
-
-# df = pd.read_csv(StringIO(csv_string))
-# print(df)
+myfunction()
